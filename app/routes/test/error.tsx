16a07e778da2dbb482c18d82f21b62d223470286
mm-masahiro <hand5.mm9.mzh@gmail.com>
2021-12-18 22:37:37 +0900

    make error boundary component

---
+++ app/routes/test/error.tsx
@@ -0,0 +1,14 @@
+export function ErrorBoundary({ error }) {
+	return (
+		<div>Error occured. {error.message}</div>
+	)
+}
+
+export default function Gists() {
+	throw new Error('Error')
+	return (
+		<div>
+			<h2>Public gists</h2>
+		</div>
+	)
+}
