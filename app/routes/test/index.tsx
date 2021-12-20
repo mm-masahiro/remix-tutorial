8b24d2c0b6dcc4090aa65cf97ffb833befef83b6
mm-masahiro <hand5.mm9.mzh@gmail.com>
2021-12-20 23:20:27 +0900

    make arrow functionla component

--- app/routes/test/index.tsx
+++ app/routes/test/index.tsx
@@ -1,5 +1,15 @@
 export default function Test() {
   return (
-    <div>testです</div>
+		<div>
+			<div>testです</div>
+			<Hoge />
+		</div>
   )
+}
+
+
+export const Hoge = () => {
+	return (
+		<div>Arrow Function Component!!</div>
+	)
 }
